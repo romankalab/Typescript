e7cde3c8a1f528fc82e03dc7b11a1f883489ae34
romankalab <roan.kalabgmail.com>
2020-10-27 16:18:25 +0100

    Hotovo

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,2 +1,6 @@
-var message:string = "Hello Appslab!"
-console.log(message)
+let height: number = 2;
+let lenght: number = 3;
+let towerheight: number = 30;
+let distance: number = 0;
+distance = (towerheight / height) * lenght;
+console.log(distance);
--- out/helloAppslabTs.js
+++ out/helloAppslabTs.js
@@ -1,3 +1,7 @@
-var message = "Hello Appslab!";
-console.log(message);
+var height = 2;
+var lenght = 3;
+var towerheight = 30;
+var distance = 0;
+distance = (towerheight / height) * lenght;
+console.log(distance);
 //# sourceMappingURL=helloAppslabTs.js.map
